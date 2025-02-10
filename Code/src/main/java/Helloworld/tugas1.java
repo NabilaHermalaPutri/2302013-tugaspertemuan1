f3594de12dc018a845a8d193defcdf2a1464d2e7
nabilahermalap <nblhrmlptr@gmail.com>
2025-02-10 13:38:18 +0700

    menambahkan code

---
+++ Code/src/main/java/Helloworld/tugas1.java
@@ -0,0 +1,8 @@
+package Helloworld;
+
+public class tugas1 {
+    public static void main(String[] args) {
+        System.out.println("Hallo World Nabila Hermala Putri dan NIM 2302013");
+        System.out.println("Saya Senang Belajar Java");
+    }
+}
